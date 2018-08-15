6150f09e2792f6dc152e388727f4faed63da2b58
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2018-08-15 11:42:42 -0400

    styling changes

--- client/src/components/users/UserTestScoresLinker.js
+++ client/src/components/users/UserTestScoresLinker.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+import { Icon } from 'semantic-ui-react'
+
+const NavBox = styled.div`
+    text-align: center;
+    margin-top: 2px;
+    box-shadow: 1px 1px 1px 1px;
+`
+
+const Wrapper = styled.div`
+    text-align: center;
+    border: 1px solid black;
+    box-shadow: 1px 1px 1px 1px;
+    margin-bottom: 20px;
+    margin-top: 20px;
+    margin-left: 20px;
+    margin-right: 25px;
+`
 
 class UserTestScoresLinker extends Component {
     state = {
@@ -22,15 +40,23 @@ class UserTestScoresLinker extends Component {
         }
         const links = this.state.tests.map((test)=>{
             return (
-                <div>
+                <Wrapper>
                     <Link to={`/users/${this.props.userId}/tests/${test.id}/graphOfScores`}>
-                        {test.name}
+                        <h1>{test.name}</h1>
+                        <Icon name="line graph" size="massive"/>
                     </Link>
-                </div>
+                </Wrapper>
             )
         })
         return (
             <div>
+                <NavBox>
+                    <Link to={`/users/${this.props.userId}`}>
+                        <div>
+                            <Icon name="home" size="big"/>
+                        </div>
+                    </Link>
+                </NavBox>
                 {links}
             </div>
         );
